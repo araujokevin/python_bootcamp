78b7b5039e3e5266e53fff1b0037e976be0f62ef
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-03-05 11:45:16 -0300

    Update task

--- day-002/05-tip-calculator.py
+++ day-002/05-tip-calculator.py
@@ -1,11 +1,12 @@
-print("Welcome to the tip calculator!")  # Mensagem antes das entradas
-
-total = float(input("What was the total bill? "))
+print("Welcome to the tip calculator!")  
+bill = float(input("What was the total bill? $"))
 tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
-total_people = int(input("How many people to split the bill? "))  # Inteiro
+people = int(input("How many people to split the bill? ")) 
+tip_as_percentage = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
 
-total_bill = total + total * (tip / 100)
-split_amount = round(total_bill / total_people, 2)
-
-print(f"Each person should pay: ${split_amount}")
+print(f"Each person should pay: ${final_amount}")
 
