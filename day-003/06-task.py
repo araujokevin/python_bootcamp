ddbcd89128b6cdea8e324ccc6f1fecc0c6c0943c
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-03-06 15:19:44 -0300

    Update task 06

--- day-003/06-task.py
+++ day-003/06-task.py
@@ -14,13 +14,13 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")    
-    elif age >= 45 and age <=55:
+    elif age >= 45 and age <=55:# Free ride for people between 45 and 55
         bill = 0
         print("Everything is going to be ok. Have a free ride on us!")
-    else:# Free ride for people between 45 and 55 
+    else: 
         bill = 12
         print("Adult tickets are $12.")
-    wants_photo = input("Do you want to have a photo take? Type Y for Yes and N for No.")
+    wants_photo = input("Do you want to have a photo taken? Type Y for Yes and N for No.")
     if wants_photo == "Y":
         # Add $3 to their bill
         bill += 3
