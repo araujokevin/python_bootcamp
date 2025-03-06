850075e15816f488b3a1fad6eff517405abd161a
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-03-06 09:22:15 -0300

    Update task 05

--- day-003/05-task.py
+++ day-003/05-task.py
@@ -1,12 +1,22 @@
-#Pizza Order Practice
+# Pizza Order Practice  
 
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+# Congratulations! You've got a job at Python Pizza! Your first task is to build an automatic pizza order program.  
+# Based on a user's order, work out their final bill. Use the input() function to get the user's preferences  
+# and then add up the total for their order to tell them how much they have to pay.  
 
-#todo: work out how much they need to pay based on theri size choice.
+# Small pizza (S): $15  
+# Medium pizza (M): $20  
+# Large pizza (L): $25  
 
-#todo: work out how much to add to their bill based on their pepperoni choice.
+# Add pepperoni for small pizza (Y or N): +$2  
 
-#todo: work out their final amount based on whether if they want extra cheese.
+print("Welcome to Python Pizza Deliveries!")  
+size = input("What size pizza do you want? S, M, or L: ")  
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")  
+extra_cheese = input("Do you want extra cheese? Y or N: ")  
+
+# TODO: Work out how much they need to pay based on their size choice.  
+
+# TODO: Work out how much to add to their bill based on their pepperoni choice.  
+
+# TODO: Work out their final amount based on whether they want extra cheese.  
